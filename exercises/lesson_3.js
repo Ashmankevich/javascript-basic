bfead8ed79310286e5ddeab094766fa4152f1e6c
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-09-25 21:23:10 +0300

    init lesson 3

---
+++ exercises/lesson_3.js
@@ -0,0 +1,24 @@
+//NORMAL level
+
+/*Task 1 💻
+Создайте переменные и присвойте им значения:
+
+    'true'
+    false
+    17
+    undefined
+    null
+Затем с помощью оператора typeof выведите в консоль все типы созданных значений переменных.
+*/
+
+let q = "true";
+let w = false;
+let e = 17;
+let r = undefined;
+let t = null;
+
+console.log(typeof q);
+console.log(typeof w);
+console.log(typeof e);
+console.log(typeof r);
+console.log(typeof t);
