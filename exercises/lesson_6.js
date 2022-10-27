b338fe91adadb02f74dec40e17a9355c188f2141
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-10-27 22:38:07 +0300

    add task of 'arr'

--- exercises/lesson_6.js
+++ exercises/lesson_6.js
@@ -201,3 +201,30 @@ function sumInput() {
   return sum;
 }
 sumInput();
+
+//Task 11
+
+/*
+Переведите текст вида border-left-width в borderLeftWidth
+важность: 5
+Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+Примеры:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
+потом переделайте всё как нужно и методом join соедините обратно.
+*/
+
+let str = "background-color";
+let arr = str.split("");
+let element = "-";
+let indexRemove = arr.indexOf(element);
+arr.splice(indexRemove, 1);
+let newArr1 = arr[indexRemove].toUpperCase();
+console.log(newArr1);
+let newArr = arr.join("");
