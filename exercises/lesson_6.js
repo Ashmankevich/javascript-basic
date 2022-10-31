14ca937f27aa44b3fdf6f8372f3121ecd543c63e
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-10-31 21:45:42 +0300

    add task about calculator

--- exercises/lesson_6.js
+++ exercises/lesson_6.js
@@ -570,3 +570,52 @@ function unique(arr) {
 }
 
 console.log(unique(strings)); // кришна, харе, :-O
+
+//Task22
+
+/*
+Создать расширяемый калькулятор
+важность: 5
+Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+
+Задание состоит из двух частей.
+
+Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+
+Пример использования:
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+
+Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+Для этой задачи не нужны скобки или сложные выражения.
+Числа и оператор разделены ровно одним пробелом.
+Не лишним будет добавить обработку ошибок.
+*/
+
+function Calculator() {
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b,
+  };
+
+  this.calculate = function (str) {
+    let split = str.split(" ");
+    let a = +split[0];
+    let b = +split[2];
+    let operator = split[1];
+    return this.methods[operator](a, b);
+  };
+}
+let calc = new Calculator();
+console.log(calc.calculate("3 + 7"));
