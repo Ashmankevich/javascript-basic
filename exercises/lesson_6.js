d471cebe1d5d4c5c1952098dbf0030e9118d8335
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-11-04 20:10:47 +0300

    add new tasks

--- exercises/lesson_6.js
+++ exercises/lesson_6.js
@@ -718,3 +718,69 @@ let keys = Array.from(map.keys());
 keys.push("more");
 
 console.log(keys);
+
+//Task 26
+
+/*
+Сумма свойств объекта
+важность: 5
+Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+
+Напишите функцию sumSalaries(salaries),
+которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+
+Если объект salaries пуст, то результат должен быть 0.
+
+Например:
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+alert( sumSalaries(salaries) );
+*/
+
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+
+function sumSalaries(obj) {
+  let result = 0;
+  if (obj == null) {
+    return null;
+  } else {
+    for (let value of Object.values(obj)) {
+      result += value;
+    }
+  }
+  return result;
+}
+sumSalaries(salaries);
+
+//Task 27
+
+/*
+Подсчёт количества свойств объекта
+важность: 5
+Напишите функцию count(obj), которая возвращает количество свойств объекта:
+
+let user = {
+  name: 'John',
+  age: 30
+};
+
+alert( count(user) )
+*/
+
+let user = {
+  name: "John",
+  age: 30,
+  city: "Minsk",
+};
+
+let count = (obj) => Object.keys(obj).length;
+count(user);
