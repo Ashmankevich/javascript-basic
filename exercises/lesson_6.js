a9411534a0d54755e43750ec422b20946c5244cc
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-11-06 20:02:30 +0300

    date as obj

--- exercises/lesson_6.js
+++ exercises/lesson_6.js
@@ -852,3 +852,106 @@ function topSalary(obj) {
   return topGuy;
 }
 topSalary(salaries29);
+
+//Task 30
+
+/*
+Создайте дату
+важность: 5
+Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут. Временная зона – местная.
+*/
+
+let date = new Date(2012, 1, 20, 3, 12);
+console.log(date);
+
+//Task 31
+
+/*
+Напишите функцию getWeekDay(date),
+показывающую день недели в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
+
+Например:
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getWeekDay(date) );        // нужно вывести "ВТ"
+*/
+
+let today = new Date(2012, 0, 3);
+
+function getWeekDay(date) {
+  let day;
+  let indexDay = date.getDay();
+  let week = {
+    1: "ПН",
+    2: "ВТ",
+    3: "СР",
+    4: "ЧТ",
+    5: "ПТ",
+    6: "СБ",
+    7: "ВС",
+  };
+
+  for (let key in week) {
+    if (key == indexDay) {
+      day = week[indexDay];
+    }
+  }
+  return day;
+}
+getWeekDay(today);
+
+//Task 32
+
+/*
+День недели в европейской нумерации
+важность: 5
+В Европейских странах неделя начинается с понедельника (день номер 1),
+затем идёт вторник (номер 2) и так до воскресенья (номер 7).
+Напишите функцию getLocalDay(date), которая возвращает «европейский» день недели для даты date.
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getLocalDay(date) );       // вторник, нужно показать 2
+
+*/
+function getLocalDay(date) {
+  let day = date.getDay();
+
+  if (day == 0) {
+    day = 7;
+  }
+
+  return day;
+}
+
+let date32 = new Date(2012, 0, 3);
+console.log(getLocalDay(date32));
+
+//Task 33
+
+/*
+Какой день месяца был много дней назад?
+важность: 4
+Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
+
+К примеру, если сегодня двадцатое число,
+то getDateAgo(new Date(), 1) вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
+
+Функция должна надёжно работать при значении days=365 и больших значениях:
+
+let date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+P.S. Функция не должна изменять переданный ей объект date.
+*/
+
+let date33 = new Date(2015, 0, 2);
+
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+  let dateMS = dateCopy.getTime();
+  let daysMS = days * 86400000;
+  return new Date(dateMS - daysMS).getDate();
+}
+getDateAgo(date33, 2);
