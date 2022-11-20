47b3c3af3c5163589c0446bcba53ec611d16287b
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-11-20 17:03:52 +0300

    NFE

--- exercises/lesson_7.js
+++ exercises/lesson_7.js
@@ -528,3 +528,39 @@ function printReverse(list) {
   }
 }
 printReverse(list);
+
+//Установка и уменьшение значения счётчика
+/*важность: 5
+Измените код makeCounter() так, чтобы счётчик мог уменьшать и устанавливать значение:
+
+counter() должен возвращать следующее значение (как и раньше).
+counter.set(value) должен устанавливать счётчику значение value.
+counter.decrease() должен уменьшать значение счётчика на 1.
+Посмотрите код из песочницы с полным примером использования.
+
+P.S. Для того, чтобы сохранить текущее значение счётчика,
+можно воспользоваться как замыканием, так и свойством функции.
+Или сделать два варианта решения: и так, и так.
+*/
+
+function makeCounter() {
+  let count = 0;
+
+  function counter() {
+    return count++;
+  }
+  counter.set = (value) => (count = value);
+
+  counter.decrease = () => count--;
+  return counter;
+}
+
+let counter = makeCounter();
+
+console.log(counter());
+console.log(counter());
+
+counter.set(10)
+counter.decrease()
+counter.decrease()
+console.log(counter())
