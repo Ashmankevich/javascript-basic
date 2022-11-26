3b16dc5c8f7658b569b6d1584040c18f7ee46888
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-11-26 13:28:02 +0300

    call, apply

--- exercises/lesson_7.js
+++ exercises/lesson_7.js
@@ -632,3 +632,33 @@ work(4, 5);
 for (let args of work.calls) {
   console.log("call:" + args.join());
 }
+
+//Задерживающий декоратор
+/*важность: 5
+Создайте декоратор delay(f, ms), который задерживает каждый вызов f на ms миллисекунд. Например:
+
+function f(x) {
+  alert(x);
+}
+
+// создаём обёртки
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000("test"); // показывает "test" после 1000 мс
+f1500("test"); // показывает "test" после 1500 мс
+Другими словами, delay(f, ms) возвращает вариант f с «задержкой на ms мс».
+
+В приведённом выше коде f – функция с одним аргументом,
+но ваше решение должно передавать все аргументы и контекст this.
+*/
+
+function delay(f, ms) {
+  return function () {
+    setTimeout(() => f.apply(this, arguments), ms);
+  };
+}
+
+let f5000 = delay(console.log, 5000);
+
+f5000("test");
