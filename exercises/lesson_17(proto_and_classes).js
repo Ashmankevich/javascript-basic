f84e148975f0d9be0d8e672c6e033a0062bb6050
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-12-01 22:33:39 +0300

    continue prototype

--- exercises/lesson_17(proto_and_classes).js
+++ exercises/lesson_17(proto_and_classes).js
@@ -50,3 +50,34 @@ let pockets = {
 
 console.log(pockets.pen);
 console.log(bed.glasses);
+
+//Создайте новый объект с помощью уже существующего
+/*важность: 5
+Представьте, что у нас имеется некий объект obj,
+созданный функцией-конструктором – мы не знаем какой именно,
+но хотелось бы создать ещё один объект такого же типа.
+
+Можем ли мы сделать так?
+
+let obj2 = new obj.constructor();
+Приведите пример функции-конструктора для объекта obj, с которой такой вызов корректно сработает.
+И пример функции-конструктора, с которой такой код поведёт себя неправильно.
+*/
+
+function Android(name) {
+  this.name = name;
+  console.log(name);
+}
+
+let android = new Android("Mr. Robot");
+let android2 = new android.constructor("Mr. T1000");
+
+function Robot(name) {
+  this.name = name;
+  console.log(name);
+}
+
+Robot.prototype = {};
+
+let robot = new Robot("Mr. Black");
+let robot2 = new robot2.constructor("Mr. White");
