a3f8524701a61cbbf2ed463c48b32878ed11275c
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-12-06 22:38:03 +0300

    add task of classes

--- exercises/lesson_17(proto_and_classes).js
+++ exercises/lesson_17(proto_and_classes).js
@@ -184,3 +184,44 @@ class Clock {
 let clock = new Clock({ wrapped: "h:m:s" });
 
 clock.start();
+
+//Ошибка создания экземпляра класса
+/*важность: 5
+В коде ниже класс Rabbit наследует Animal.
+
+К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+class Animal {
+
+   constructor(name) {
+     this.name = name;
+   }
+ 
+ }
+ 
+ class Rabbit extends Animal {
+   constructor(name) {
+     this.name = name;
+     this.created = Date.now();
+   }
+ }
+ 
+ let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+ alert(rabbit.name);
+ */
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик");
+alert(rabbit.name);
