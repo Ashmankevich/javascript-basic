c4a96e8454a164390e9ae84778939ce19b8b6033
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-01-01 20:09:06 +0300

    codewars

---
+++ codewars/6kyu/HighestScoringWord.js
@@ -0,0 +1,39 @@
+/*
+DESCRIPTION:
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+
+function high(x) {
+  let arr = x.toLowerCase().split(" ");
+  let arrNumber = arr.map(function (item) {
+    let sum = 0;
+    for (let key of item) sum += key.charCodeAt(0) - 96;
+    return sum;
+  });
+
+  let getMaxOfArray = function (arrNumber) {
+    return Math.max.apply(null, arrNumber);
+  };
+
+  let index = arrNumber.indexOf(getMaxOfArray(arrNumber));
+  let word = arr[index];
+
+  return word;
+}
+high("man i need a taxi up to ubud");
+
+//best practise
+function high(s) {
+  let as = s
+    .split(" ")
+    .map((s) => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
+  return s.split(" ")[as.indexOf(Math.max(...as))];
+}
