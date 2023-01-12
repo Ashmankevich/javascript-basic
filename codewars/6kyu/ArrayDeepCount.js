78cb916080fd0b9d1ad5a64694c5e064dbea1e21
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-01-13 00:27:05 +0300

    codewars:'Array Deep Count' refactor

--- codewars/6kyu/ArrayDeepCount.js
+++ codewars/6kyu/ArrayDeepCount.js
@@ -11,10 +11,21 @@ Examples
 The input will always be an array.
 */
 
+// first solution
 function deepCount(a) {
   return a.reduce(
     (ac, cur) => ac + (Array.isArray(cur) ? deepCount(cur) : null),
     a.length
   );
+}
+deepCount([1, 2, [3, 4, [5]]]);
+
+// second solution
+function deepCount(a) {
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    Array.isArray(a[i]) == false ? null : (result += deepCount(a[i]));
+  }
+  return a.length + result;
 }
 deepCount([1, 2, [3, 4, [5]]]);
