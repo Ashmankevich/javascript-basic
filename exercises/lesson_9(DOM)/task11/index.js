998702750957cd4808e907231a7546cb1564e725
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-01-23 20:09:28 +0300

    DOM:task11

---
+++ exercises/lesson_9(DOM)/task11/index.js
@@ -0,0 +1,23 @@
+function showNotification({ top = 0, right = 0, className, html }) {
+  let div = document.createElement("div");
+  div.className = "notification";
+
+  div.style.top = top + "px";
+  div.style.right = right + "px";
+  div.innerHTML = html;
+  div.classList.add(className);
+
+  document.body.append(div);
+
+  setTimeout(() => div.remove(), 1000);
+}
+
+let i = 1;
+setInterval(() => {
+  showNotification({
+    top: 10,
+    right: 10,
+    html: "Hello " + i++,
+    className: "welcome",
+  });
+}, 1500);
