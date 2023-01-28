7007bfe241a56a334c9d2a75b99ba71261d24035
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-01-28 13:05:48 +0300

    DOM:task 15, fix bug

--- exercises/lesson_9(DOM)/task15/index.js
+++ exercises/lesson_9(DOM)/task15/index.js
@@ -2,14 +2,18 @@ let elemCoords = document.getElementById("coords");
 let coords = elemCoords.getBoundingClientRect();
 
 let elemField = document.getElementById("field");
+let field = elemField.getBoundingClientRect();
 
 let answer1 = [coords.left, coords.top];
-let answer2 = [coords.right, coords.bottom];
+let answer2 = [coords.left + coords.width, coords.top + coords.height];
 let answer3 = [
   coords.left + elemField.clientLeft,
   coords.top + elemField.clientTop,
 ];
-let answer4;
+let answer4 = [
+  coords.right - elemField.clientLeft,
+  coords.bottom - elemField.clientTop,
+];
 
 alert(answer1);
 alert(answer2);
