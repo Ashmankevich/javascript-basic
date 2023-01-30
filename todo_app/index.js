98f3b47a77f04f122a3182afa71771eb93066a4b
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-01-30 21:49:03 +0300

    todo_app, component styling

--- todo_app/index.js
+++ todo_app/index.js
@@ -31,28 +31,15 @@ const additionalItems = document.createElement("div");
 additionalItems.className = "wrapper addition-items";
 container.append(additionalItems);
 
-const btnShowCompleted = document.createElement("button");
-btnShowCompleted.className = "btn show-completed";
-btnShowCompleted.textContent = "Show Completed";
-additionalItems.append(btnShowCompleted);
-
-const btnShowAll = document.createElement("button");
-btnShowAll.className = "btn show-all";
-btnShowAll.textContent = "Show All";
-additionalItems.append(btnShowAll);
-
-const inputSearchTODO = document.createElement("input");
-inputSearchTODO.className = "input__search-todo";
-inputSearchTODO.setAttribute("placeholder", "Search...");
-additionalItems.append(inputSearchTODO);
-
 const divAllCount = document.createElement("div");
 divAllCount.className = "wrapper__all-count";
 additionalItems.append(divAllCount);
 const allCountName = document.createElement("p");
+allCountName.className = "text-count-name";
 allCountName.textContent = "All:";
 divAllCount.append(allCountName);
 const allCount = document.createElement("p");
+allCount.className = "text-all-count";
 allCount.textContent = "0";
 divAllCount.append(allCount);
 
@@ -60,8 +47,25 @@ const completed = document.createElement("div");
 completed.className = "wrapper__completed";
 additionalItems.append(completed);
 const allCompletedName = document.createElement("p");
+allCompletedName.className = "text-completed-name";
 allCompletedName.textContent = "Completed:";
 completed.append(allCompletedName);
 const allCompleted = document.createElement("p");
+allCompleted.className = "text-all-completed";
 allCompleted.textContent = "2";
 completed.append(allCompleted);
+
+const btnShowAll = document.createElement("button");
+btnShowAll.className = "btn show-all";
+btnShowAll.textContent = "Show All";
+additionalItems.append(btnShowAll);
+
+const btnShowCompleted = document.createElement("button");
+btnShowCompleted.className = "btn show-completed";
+btnShowCompleted.textContent = "Show Completed";
+additionalItems.append(btnShowCompleted);
+
+const inputSearchTODO = document.createElement("input");
+inputSearchTODO.className = "input__search-todo";
+inputSearchTODO.setAttribute("placeholder", "Search...");
+additionalItems.append(inputSearchTODO);
