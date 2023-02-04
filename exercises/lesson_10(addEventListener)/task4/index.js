dfc1c63e4759965faf508cc4902c03c2544468f6
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2023-02-04 10:29:33 +0300

    fix task 4 of lesson 10

--- exercises/lesson_10(addEventListener)/task4/index.js
+++ exercises/lesson_10(addEventListener)/task4/index.js
@@ -1,7 +1,7 @@
 let ball = document.getElementById("ball");
 let field = document.getElementById("field");
 
-function handler() {
+function moveBall(event) {
   let fieldCoords = this.getBoundingClientRect();
   let ballCoords = {
     top:
@@ -11,10 +11,29 @@ function handler() {
       fieldCoords.left -
       field.clientLeft -
       ball.clientWidth / 2,
+    bottom:
+      fieldCoords.bottom -
+      event.clientY -
+      field.clientTop -
+      ball.clientHeight / 2,
+    right:
+      fieldCoords.right -
+      event.clientX -
+      field.clientLeft -
+      ball.clientWidth / 2,
   };
+
+  ballCoords.top < 0 ? (ballCoords.top = 0) : null;
+  ballCoords.left < 0 ? (ballCoords.left = 0) : null;
+  ballCoords.bottom < 0
+    ? (ballCoords.top = field.clientHeight - ball.clientHeight)
+    : null;
+  ballCoords.right < 0
+    ? (ballCoords.left = field.clientWidth - ball.clientWidth)
+    : null;
 
   ball.style.left = ballCoords.left + "px";
   ball.style.top = ballCoords.top + "px";
 }
 
-field.addEventListener("click", handler);
+field.addEventListener("click", moveBall);
